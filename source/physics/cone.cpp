196b887a8cb413a6b9f9a964d384104486631df1
zmmyslony <zmmyslony@gmail.com>
2024-04-05 19:56:00 +0200

    Correction of interaction force calculation: friction didn't use the correct force for magnitude calculation.

--- source/physics/cone.cpp
+++ source/physics/cone.cpp
@@ -63,16 +63,19 @@ double Cone::addInteractionForce(const Eigen::Vector3d &pos, Eigen::Vector3d &no
     Eigen::Vector3d cone_normal = coneNormal(pos);
     double distance_v = relative_position.dot(cone_normal);
     if (distance_v >= 0) { return 0; }
-    double normal_force = distance_v * force_prefactor * shear_modulus * thickness;
-    double friction_force = normal_force * friction_coefficient;
+    double normal_response_force = -distance_v * force_prefactor * shear_modulus * thickness;
 
-    Eigen::Vector3d tangent_force = node_force - node_force.dot(cone_normal) * cone_normal;
+    Eigen::Vector3d normal_force = node_force.dot(cone_normal) * cone_normal;
+    Eigen::Vector3d tangent_force = node_force - normal_force;
+
+    double friction_force = normal_force.norm() * friction_coefficient;
     double tangent_force_v = tangent_force.norm();
+
     if (tangent_force_v <= friction_force) {
         node_force -= tangent_force;
     } else {
         node_force -= tangent_force.normalized() * friction_force;
     }
-    node_force -= normal_force * cone_normal;
-    return normal_force;
+    node_force += normal_response_force * cone_normal;
+    return normal_response_force;
 }
--- source/physics/slide.cpp
+++ source/physics/slide.cpp
@@ -30,20 +30,20 @@ void Slide::initialise(const std::vector<Eigen::Vector3d> &node_pos, double dial
 
 double Slide::addInteractionForce(const Eigen::Vector3d &pos, Eigen::Vector3d &node_force, double shear_modulus,
                                   double thickness) const {
-    Eigen::Vector3d interaction = {0, 0, 0};
     double distance_v = distance(pos);
     if (distance_v >= 0) { return 0; }
-    double normal_force = distance_v * force_prefactor * shear_modulus * thickness;
-    double friction_force = normal_force * friction_coefficient;
+    double normal_response_force = -distance_v * force_prefactor * shear_modulus * thickness;
 
-    Eigen::Vector3d tangent_force = node_force - node_force.dot(normal) * normal;
+    Eigen::Vector3d normal_force = node_force.dot(normal) * normal;
+    Eigen::Vector3d tangent_force = node_force - normal_force;
+    double friction_force = normal_force.norm() * friction_coefficient;
     double tangent_force_v = tangent_force.norm();
     if (tangent_force_v <= friction_force) {
         node_force -= tangent_force;
     } else {
         node_force -=  tangent_force.normalized() * friction_force;
     }
-    node_force -= normal_force * normal;
-    return normal_force;
+    node_force += normal_response_force * normal;
+    return normal_response_force;
 }
 
