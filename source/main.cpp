19fa5a99c73ba9cba9a58661dd95a94baa1ec994
zmmyslony <zmmyslony@gmail.com>
2025-11-13 09:53:52 +0000

    Implementation of multithreading benchmark

--- source/main.cpp
+++ source/main.cpp
@@ -75,9 +75,84 @@ maxima (over the mesh) of non-dimensionalised node speed and elastic force.
 
 #define _USE_MATH_DEFINES
 
+#include <iomanip>
+
 #include "simulation.h"
 
-int main(int argc, char *argv[]) {
-    Simulation simulation(argc, argv);
-    return simulation.run_simulation();
+std::pair<double, double> find_timing_fit(const std::vector<long long>& thread_timings)
+{
+    /** Fits a function: t_constant + t_parallel / num_threads, to a list of
+     * timings.
+     **/
+
+    double sum = 0;
+    double sum_inverse = 0;
+    double nInverse = 0;
+    double nInverseSquared = 0;
+    auto n = static_cast<double>(thread_timings.size());
+    for (int i = 0; i < thread_timings.size() - 1; i++)
+    {
+        sum += static_cast<double>(thread_timings[i]);
+        sum_inverse += static_cast<double>(thread_timings[i] / (i + 1));
+        nInverse += 1. / (i + 1);
+        nInverseSquared += 1. / pow(i + 1, 2);
+    }
+    double denominator = n * nInverseSquared - nInverse * nInverse;
+    double constant_duration = (nInverseSquared * sum - nInverse * sum_inverse) / denominator;
+    double parallel_duration = (-nInverse * sum + n * sum_inverse) / denominator;
+
+    return {constant_duration, parallel_duration};
+}
+
+void benchmark_multithreading(int argc, char* argv[], unsigned short max_thread_count)
+{
+    std::vector<long long> full_simulation_durations;
+    std::vector<long long> mechanics_simulation_durations;
+    std::vector<long long> exporting_simulation_durations;
+    std::vector<long long> unaccounted_simulation_durations;
+
+    for (int i = 1; i <= max_thread_count; i++)
+    {
+        Simulation simulation = Simulation(argc, argv, i);
+        simulation.run_simulation();
+        full_simulation_durations.push_back(simulation.benchmarking_full_duration / 1000);
+        mechanics_simulation_durations.push_back(simulation.benchmarking_mechanics_duration / 1000);
+        exporting_simulation_durations.push_back(simulation.benchmarking_export_duration / 1000);
+        unaccounted_simulation_durations.push_back(
+            (simulation.benchmarking_full_duration - simulation.benchmarking_mechanics_duration - simulation.
+                benchmarking_export_duration) / 1000);
+    }
+
+
+    std::pair<double, double> durations_full = find_timing_fit(full_simulation_durations);
+    std::pair<double, double> duration_mechanics = find_timing_fit(mechanics_simulation_durations);
+    std::pair<double, double> duration_exporting = find_timing_fit(exporting_simulation_durations);
+    std::pair<double, double> duration_unaccounted = find_timing_fit(unaccounted_simulation_durations);
+
+
+    std::cout << std::endl;
+    std::cout << std::defaultfloat << std::setprecision(2);
+    std::cout << "========================================" << std::endl;
+    std::cout << "======= Benchmark results below ========" << std::endl;
+    std::cout << "========================================" << std::endl;
+
+    double mechanics_ratio = (duration_mechanics.first + duration_mechanics.second) / (durations_full.first +
+        durations_full.second) * 100;
+    double export_ratio = (duration_exporting.first + duration_exporting.second) / (durations_full.first +
+        durations_full.second)* 100;
+    double unaccounted_ratio = (duration_unaccounted.first + duration_unaccounted.second) / (durations_full.first +
+        durations_full.second) * 100;
+
+    std::cout << "Category \tamount [%] \toverhead [ms] \tt_parallel [ms]\tparallelisation [%]" << std::endl;
+    std::printf("Total \t\t%.2f \t\t%.0f \t\t\t%.0f \t\t\t%.2f\n", 100., durations_full.first, durations_full.second, (durations_full.second) / (durations_full.first + durations_full.second) * 100);
+    std::printf("Mechanics \t%.2f \t\t%.0f \t\t\t%.0f \t\t\t%.2f\n", mechanics_ratio, duration_mechanics.first, duration_mechanics.second, (duration_mechanics.second) / (duration_mechanics.first + duration_mechanics.second) * 100);
+    std::printf("Exporting \t%.2f \t\t%.0f \t\t\t%.0f \t\t\t%.2f\n", export_ratio, duration_exporting.first, duration_exporting.second, (duration_exporting.second) / (duration_exporting.first + duration_exporting.second) * 100);
+    std::printf("Unaccounted\t%.2f \t\t%.0f \t\t\t%.0f \t\t\t%.2f\n", unaccounted_ratio, duration_unaccounted.first, duration_unaccounted.second, (duration_unaccounted.second) / (duration_unaccounted.first + duration_unaccounted.second) * 100);
+}
+
+int main(int argc, char* argv[])
+{
+    // Simulation simulation(argc, argv, -1);
+    // return simulation.run_simulation();
+    benchmark_multithreading(argc, argv, 8);
 }
